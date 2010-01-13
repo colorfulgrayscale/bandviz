5774f5800318ddccee74d2e735d99471198ccdd6
colorfulgrayscale <colorfulgrayscale@gmail.com>
2010-01-13 15:21:01 -0600

    first commit

---
+++ artist.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+class Artist:
+    def __init__(self, name):
+        print "asf" + __name__;
+        self.name = name;
+
+if __name__ == '__main__':
+    bob = Artist('Bob Smith')
+    print(bob.name)
+
---
+++ driver.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import artist;
+
+bob = artist.Artist('Bob Smith');
+print bob.name;
+
