cf2632523d4a37775d06eb574f85cf7444a6f926
colorfulgrayscale <colorfulgrayscale@gmail.com>
2010-01-15 12:12:46 -0600

    checkpoint,band members needs work

--- wikiparser.py
+++ wikiparser.py
@@ -6,15 +6,78 @@ import re
 import BeautifulSoup
 
 
-
+class WikiParser:
+    def __init__(self,hyperlink):
+        self.http = httplib2.Http() #initialize http2 library
+        self.data = str(self.http.request(hyperlink)) #get html data from page
+    def setLink(self,link): 
+        self.data = str(self.http.request(hyperlink)) #get html data from page
+    def getRelatedBands(self):
+        soup = BeautifulSoup.BeautifulSoup(self.data) 
+        tableData =  str(soup.findAll("table", { "class" : "infobox vcard" })) #find info box
+        soup = BeautifulSoup.BeautifulSoup(tableData)
+        rowData = str(soup.findAll("tr")) #get all rows
+        soup = BeautifulSoup.BeautifulSoup(rowData)
+        relatedBands = "" 
+        relatedBandsDictionary = {'':''} #init return dictionary
+        for i in soup.contents:
+            value = str(i)
+            if (not value.strip() ==",") and (not value.strip() =="]") and (not value.strip() =="[") : #if data we are looking for
+                soup2 = BeautifulSoup.BeautifulSoup(value)
+                thData = str(soup2.findAll("th")) #find all header cells
+                output  = re.compile('>(.*?)</', re.DOTALL | re.IGNORECASE).findall(str(thData)) #get text in header cell
+                if "associated acts" in str(output).lower(): 
+                    tdData = str(soup2.findAll("td")) #get all cells
+                    soup3 = BeautifulSoup.BeautifulSoup(tdData)
+                    aData = str(soup3.findAll("a")) #get all links in cell
+                    relatedBands  = re.compile('\">(.*?)</a>', re.DOTALL | re.IGNORECASE).findall(str(aData)) #strip text
+                    relatedBandLinks  = re.compile('href=\"(.*?)title', re.DOTALL | re.IGNORECASE).findall(str(aData)) #strip link
+                    for bandLinks in relatedBandLinks:
+                        bandLinksIndex = relatedBandLinks.index(bandLinks)
+                        relatedBandLinks[bandLinksIndex] = "http://en.wikipedia.org" + bandLinks
+                        relatedBandsDictionary[relatedBands[bandLinksIndex]] = relatedBandLinks[bandLinksIndex] #push into dictionary
+                    return relatedBandsDictionary
+        return relatedBandsDictionary
+    def getBandMembers(self):
+        soup = BeautifulSoup.BeautifulSoup(self.data) 
+        tableData =  str(soup.findAll("table", { "class" : "infobox vcard" })) #find info box
+        soup = BeautifulSoup.BeautifulSoup(tableData)
+        rowData = str(soup.findAll("tr")) #get all rows
+        soup = BeautifulSoup.BeautifulSoup(rowData)
+        relatedBands = "" 
+        relatedBandsDictionary = {'':''} #init return dictionary
+        for i in soup.contents:
+            value = str(i)
+            valueIndex = soup.contents.index(i)
+            if (not value.strip() ==",") and (not value.strip() =="]") and (not value.strip() =="[") : #if data we are looking for
+                soup2 = BeautifulSoup.BeautifulSoup(value)               
+                thData = str(soup2.findAll("th")) #find all header cells
+                output  = re.compile('>(.*?)</', re.DOTALL | re.IGNORECASE).findall(str(thData)) #get text in header cell
+                print str(output) + " ** " + str(soup2.contents) + ">>" + str(valueIndex) +  " >>\n---\n"
+                if "['members']" == str(output).lower():
+                    nextRow = str(soup.contents[valueIndex+1])
+                    newIndex = valueIndex+1
+                    print "!!MemberText:" + str(valueIndex) + "-" + str(++i) + "!!!\n\n"
+                    soupPlus1 = BeautifulSoup.BeautifulSoup(nextRow)
+                    #soup.contents[valueIndex+2].extract()
+                    tdData = str(soupPlus1.findAll("td")) #get all cells
+                    soup3 = BeautifulSoup.BeautifulSoup(tdData)
+                    print "^^^" + str(tdData)
+                    aData = str(soup3.findAll("a")) #get all links in cell
+                    relatedBands  = re.compile('\">(.*?)</a>', re.DOTALL | re.IGNORECASE).findall(str(aData)) #strip text
+                    relatedBandLinks  = re.compile('href=\"(.*?)title', re.DOTALL | re.IGNORECASE).findall(str(aData)) #strip link
+                    return
+                
+        return relatedBandsDictionary
+                
+        
+a = WikiParser("http://en.wikipedia.org/wiki/Soundgarden")
+a.getBandMembers()
+#hyperlink = "http://en.wikipedia.org/wiki/Adam_Jones_(musician)"
 
 #http = httplib.HTTP()
 
-http = httplib2.Http()
-data = str(http.request('http://en.wikipedia.org/wiki/Adam_Jones_%28musician%29'))
 
-soup = BeautifulSoup.BeautifulSoup(data)
-data = str(soup.prettify())
 
 """
 filename = "output.html"
@@ -25,17 +88,6 @@ file.close()
 
 #print data
 
-tableData =  str(soup.findAll("table", { "class" : "infobox vcard" }))
-soup = BeautifulSoup.BeautifulSoup(tableData)
-data = str(soup.prettify())
-
-rowData = str(soup.findAll("tr"))
-soup = BeautifulSoup.BeautifulSoup(rowData)
-#print rowData;
-for i in soup.contents:
-    value = str(i)
-    if (not value.strip() ==",") and (not value.strip() =="]") and (not value.strip() =="[") :
-        print  value + "\n-------------\n"
         
     
     
